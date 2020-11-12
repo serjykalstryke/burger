3b79406f578530e38226c0c2e6d5e0d6d17e2852
David Stinnett <davidstinnett@icloud.com>
2020-11-12 14:39:13 -0500

    progress update

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 const orm = require("../db/orm");
 
+const burger = {
+    all: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger
