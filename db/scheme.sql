a7a410fc807d0d8041db7b61106e9c65a9d05d20
David Stinnett <davidstinnett@icloud.com>
2020-11-12 13:29:17 -0500

    added scheme file

---
+++ db/scheme.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burget_db;
+USE burger_db
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL
+    
+)
