0e4e5e0d920bef750c6cc3681e5dbe89ce8ea84f
David Stinnett <davidstinnett@icloud.com>
2020-11-14 17:07:04 -0500

    hopefully fixed heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 const mysql = require("mysql");
-const connection
 
 if (process.env.JAWSDB_URL) {
-        connection = mysql.createConnection(process.env.JAWSDB_URL);
+       let connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-        connection = mysql.createConnection({
+      let connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
