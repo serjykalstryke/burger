2da8ebc13dfa68994cbaf40f7e9c5a5b3302a9de
David Stinnett <davidstinnett@icloud.com>
2020-11-14 16:34:50 -0500

    added code to allow databse to function in heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password:"Manicweedman22593!",
-    database:"burger_db"
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Manicweedman22593!",
+        database: "burger_db"
+    })
+};
 
 connection.connect((err) => {
-    if(err) {
+    if (err) {
         console.error("error connecting: " + err.stack);
         return;
     }
