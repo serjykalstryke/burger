ecb3e86940baf8673acb612e5a773ecae63b7946
David Stinnett <davidstinnett@icloud.com>
2020-11-15 13:05:53 -0500

    heroku....

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
     burger.all((data) => {
         let hbsObject = {
-            burgers: data
+            burgers: data,
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
